999a5a0c8664672cbb03c38faac18478300c98aa
tkrikoriam <tony.krikorian@outlook.com>
2020-10-07 18:37:37 -0300

    zzzzzz

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,4 +1,4 @@
-//import axios from "axios";
+import axios from "axios";
 import {
   GET_LANGUAGES_INIT,
   GET_LANGUAGES_SUCCESS,
@@ -8,17 +8,13 @@ import {
 export const getTranslationLanguages = () => async (dispatch) => {
   dispatch({ type: GET_LANGUAGES_INIT });
 
-  // axios
-  //   .get("http://internal-service:4000.microservices/translate/languages")
-  //   .then((response) => {
-  //     console.log(response);
-  //     dispatch({ type: GET_LANGUAGES_SUCCESS, payload: response.data });
-  //   })
-  fetch({
-    url: "http://internal-service:4000.microservices/translate/languages",
-    method: "GET",
-    mode: "cors",
-  })
+  axios
+    .get("http://internal-service:4000.microservices/translate/languages")
+    .then((response) => {
+      console.log(response);
+      dispatch({ type: GET_LANGUAGES_SUCCESS, payload: response.data });
+    })
+
     .then((response) => {
       console.log(response);
       dispatch({ type: GET_LANGUAGES_SUCCESS, payload: response.data });
