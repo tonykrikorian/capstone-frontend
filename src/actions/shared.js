7c6e13697d7f7f99e956a99a480fb15532e91a31
tkrikoriam <tony.krikorian@outlook.com>
2020-10-07 18:07:46 -0300

    Add uiuiuixxxx

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -8,12 +8,22 @@ import {
 export const getTranslationLanguages = () => async (dispatch) => {
   dispatch({ type: GET_LANGUAGES_INIT });
 
-  axios
-    .get("http://internal-service:4000.microservices/translate/languages")
+  // axios
+  //   .get("http://internal-service:4000.microservices/translate/languages")
+  //   .then((response) => {
+  //     console.log(response);
+  //     dispatch({ type: GET_LANGUAGES_SUCCESS, payload: response.data });
+  //   })
+  fetch({
+    url: "http://internal-service:4000.microservices/translate/languages",
+    method: "GET",
+    mode: "cors",
+  })
     .then((response) => {
       console.log(response);
       dispatch({ type: GET_LANGUAGES_SUCCESS, payload: response.data });
     })
+
     .catch((error) => {
       console.log(error);
       dispatch({ type: GET_LANGUAGES_FAILURE, payload: error });
