776a01908e21a388232bb258b32c673f8276af6f
tkrikoriam <tony.krikorian@outlook.com>
2020-10-07 17:36:47 -0300

    Add uiuiui

--- src/actions/TranslationAction.js
+++ src/actions/TranslationAction.js
@@ -21,7 +21,7 @@ export const translateTextAction = (
   dispatch({ type: MAKE_TRANSLATION_INIT });
   axios
     .post(
-      process.env.REACT_APP_TRANSLATE,
+      "http://internal-service:4000.microservices/api/translate",
       translateTextRequest(message, fromLanguage, toLanguage),
       {}
     )
