83aef60a329965b147e420d338095432d3a99cc3
tkrikoriam <tony.krikorian@outlook.com>
2020-10-09 19:09:59 -0300

    Add quitar archivos

--- docker_build.sh
+++
@@ -1,6 +0,0 @@
-#Execute docker build 
-
-docker build -t tkrikoriam/translation-frontend:v9 . #--build-arg getlanguages="http://localhost:3500/api/translate/languages"  --build-arg urltranslate="http://localhost:3500/api/translate" 
-
-#Docker run
-#docker run -d -p3501:80 --name translation-frontend tkrikoriam/translation-frontend:v5
--- docker_upload.sh
+++
@@ -1,5 +0,0 @@
-#Loguearse en docker
-docker login
-
-#Subir la imagen
-docker push tkrikoriam/translation-frontend:v5
